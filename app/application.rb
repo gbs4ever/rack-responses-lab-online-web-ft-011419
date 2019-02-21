10a019087f8c7ed3052347f7f4f7894257138625
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-21 18:48:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,13 @@
 class Application
 
-def call(env)
-resp= Rack::Response.new
+  def call(env)
+  resp= Rack::Response.new
+      if time < 12
+        resp.write  "Good Morning!"
+      else
+        resp.write     "Good Afternoon!"
 
-  if time < 12
- resp.write  "Good Morning!"
-  else
-resp.write     "Good Afternoon!"
-
+      end
+      resp.finish
   end
-resp.finish
-end
 end
